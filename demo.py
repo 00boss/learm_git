911cd78e702ac0d67a89cac3680db32888d1428f
learm <1440915535@qq.com>
2022-07-23 16:18:33 +0800

    v1

---
+++ demo.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 18 15:38:17 2022
+
+@author: admin
+"""
+
+import sys
+import os
+
+#输出 可执行文件/py文件，所在的位置
+print(sys.argv)
+
+print(sys.argv[0])
+
+#找到差数的绝对路劲
+print(os.path.realpath(sys.argv[0]))
